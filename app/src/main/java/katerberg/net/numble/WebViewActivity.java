d7e19b01c8ded91e71f6ce2e08359636903c003b
Mark Katerberg <katerberg@fastmail.fm>
2016-08-26 14:57:36 -0500

    Initial release

---
+++ app/src/main/java/katerberg/net/numble/WebViewActivity.java
@@ -0,0 +1,26 @@
+package katerberg.net.numble;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.numblegame.com/dist/index.html");
+
+    }
+
+}
